2cfc476dbcd1c9c0c2c0e730843bee14624b77f2
Seagull <904212935@qq.com>
2023-10-11 18:50:48 +0800

    index

--- 前端/js/index.js
+++ 前端/js/index.js
@@ -1,3 +1,25 @@
 const user = document.querySelector('.user')
-const adm = document.querySelector('administrators')
+const adm = document.querySelector('.administrators')
+const card = document.querySelector('.card')
+const back = document.querySelector('.back')
+const card2 = document.querySelector('.card2')
+const back2 = document.querySelector('.back2')
+
+
+user.addEventListener('click',function(){
+    card.style.transform = `rotateY(180deg)`
+})
+back.addEventListener('click',function(e){
+    if(e.target.dataset.id!='inp'&&e.target.tagName != 'BUTTON'&&e.target.tagName != 'A'){
+        card.style.transform = `rotateY(0deg)`
+    }
+})
+adm.addEventListener('click',function(){
+    card2.style.transform = `rotateY(180deg)`
+})
+back2.addEventListener('click',function(e){
+    if(e.target.dataset.id!='inp'&&e.target.tagName != 'BUTTON'&&e.target.tagName != 'A'){
+        card2.style.transform = `rotateY(0deg)`
+    }
+})
 
